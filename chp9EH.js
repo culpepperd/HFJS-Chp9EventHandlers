aa1446b508632227cb4f5e6f94fd37a1537b3b7e
culpepperd <culpepperd2490@gmail.com>
2023-08-11 17:46:24 -0400

    using event handlers and .onclick

---
+++ chp9EH.js
@@ -0,0 +1,10 @@
+window.onload = init;
+function init() {
+	var image = document.getElementById("zero");
+	image.onclick = showAnswer;
+}
+
+function showAnswer() {
+	var image = document.getElementById("zero");
+	image.src = "images/zero.jpg";
+}
